e7a026e4791aa8d7eab9225b4e262fc5477f15fc
FC5570 <68158483+FC5570@users.noreply.github.com>
2021-02-10 16:11:21 +0530

    lint

--- src/commands/Fun/porn.js
+++ src/commands/Fun/porn.js
@@ -1,27 +1,27 @@
-const { Command } = require('discord-akairo')
+const { Command } = require("discord-akairo");
 
 class kek extends Command {
-    constructor() {
-        super('porn', {
-            aliases: ['porn'],
-            description: {
-                content: "😏",
-                usage: "porn [request/query]",
-                examples: [
-                    "porn",
-                ]
-            },
-            category: "Fun",
-            clientPermissions: ['SEND_MESSAGES'],
-            userPermissions: [],
-            cooldown: 2000,
-            ratelimit: 1,
-        })
-    }
+  constructor() {
+    super("porn", {
+      aliases: ["porn"],
+      description: {
+        content: "😏",
+        usage: "porn [request/query]",
+        examples: ["porn"],
+      },
+      category: "Fun",
+      clientPermissions: ["SEND_MESSAGES"],
+      userPermissions: [],
+      cooldown: 2000,
+      ratelimit: 1,
+    });
+  }
 
-    async exec(message) {
-        return message.channel.send("you really thought you would get porn didnt you, you greedy bitch")
-    }
+  async exec(message) {
+    return message.channel.send(
+      "you really thought you would get porn didnt you, you greedy bitch"
+    );
+  }
 }
 
-module.exports = kek
+module.exports = kek;
