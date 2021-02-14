b5330790c8938dbeb3ae3b164fc1fa306e751fa9
FC5570 <68158483+FC5570@users.noreply.github.com>
2021-02-14 18:21:30 +0530

    😏

--- src/commands/Fun/porn.js
+++ src/commands/Fun/porn.js
@@ -1,4 +1,5 @@
 const { Command } = require("discord-akairo");
+const { MessageAttachment } = require('discord.js')
 
 class kek extends Command {
   constructor() {
@@ -18,9 +19,18 @@ class kek extends Command {
   }
 
   async exec(message) {
-    return message.channel.send(
-      "you really thought you would get porn didnt you, you greedy bitch"
-    );
+    const delay = ms => new Promise(res => setTimeout(res, ms));
+    const attach = new MessageAttachment("https://i.imgur.com/UYwVVyU.gif", "SPOILER_you_greedy_bitch.gif")
+
+    try {
+    message.member.send(attach)
+    await delay(7000)
+    message.member.send('get rickrolled you greedy bitch')
+    message.channel.send("check your dms 😏")
+    } catch(e) {
+      console.log(e)
+      return message.channel.send("how do you expect me to send porn to you if your dms are not enabled smh")
+    }
   }
 }
 
